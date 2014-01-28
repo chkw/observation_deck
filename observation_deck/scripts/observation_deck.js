fd85e7478ef5b79df881540bd4efa3974c07f376
chrisw <chrisw@soe.ucsc.edu>
2014-01-27 16:05:32 -0800

    read in rows as matrix data

--- observation_deck/scripts/observation_deck.js
+++ observation_deck/scripts/observation_deck.js
@@ -7,23 +7,55 @@
 var countsUrl = "observation_deck/data/gene_count.tab";
 var panelUrl = "observation_deck/data/gene.tab";
 
+/*
+ * Synchronous GET
+ */
+function getResponse(url) {
+    var xhr = null;
+    xhr = new XMLHttpRequest();
+    xhr.open("GET", url, false);
+    xhr.onload = function() {
+        var status = xhr.status;
+        if (status != 200) {
+            console.log("xhr error: " + status);
+        }
+    };
+    xhr.send(null);
+    var response = xhr.responseText;
+
+    return response;
+}
+
+/**
+ * Get a pretty JSON.
+ */
+function prettyJson(object) {
+    return JSON.stringify(object, null, '\t');
+}
+
 /**
  * get the JSON data to create a heatmapData object.
  */
 function setObservationData(url) {
-    d3.tsv(url, function(d) {
-        console.log(d);
-        return {
-            "sample" : "" + d.Group,
-            "event" : "" + d.Workflow,
-            "value" : "" + d.State
-        };
-    }, function(error, data) {
-        var settings = {
-        };
-
-        var dataObj = new observationData(data, settings);
+    var response = getResponse(url);
+    var parsedResponse = d3.tsv.parse(response, function(d) {
+        var event = d["#GENE"];
+        var rowData = new Array();
+        for (var sample in d) {
+            if (sample != "#GENE") {
+                var value = d[sample];
+                var data = {
+                    "sample" : sample,
+                    "event" : event,
+                    "value" : +value
+                };
+                rowData.push(data);
+            }
+        }
+        return rowData;
     });
+
+    console.log("parsedResponse->" + prettyJson(parsedResponse));
 }
 
 // TODO onload
