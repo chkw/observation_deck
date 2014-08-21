901ccf6222cd288980e862629a04b3d1759bfa61
chrisw <chrisw@soe.ucsc.edu>
2014-08-21 14:24:10 -0700

    change in environment to use virtualenv

--- start_tornado.sh
+++ start_tornado.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+PYTHON=`which python`
 
 PATH="./"
 
 PORT="9887"
 
 echo "server listening on port $PORT"
-./static_server.py $PATH $PORT
+$PYTHON ./static_server.py $PATH $PORT
--- static_server.py
+++ static_server.py
@@ -1,22 +1,13 @@
-#!/usr/bin/python
-
 import tornado.ioloop
 import tornado.web
 import tornado.httpserver
-import urllib
-import datetime
-import base64
-import time
-import json
-import re
-import time
 import sys
-from copy import deepcopy
 
 if __name__ == "__main__":
-    application = tornado.web.Application([
-        (r"/(.*)", tornado.web.StaticFileHandler, {"path": sys.argv[1]}),
-    ])
-    http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(sys.argv[2])
-    tornado.ioloop.IOLoop.instance().start()
+	path = sys.argv[1]
+	port = sys.argv[2]
+	
+	application = tornado.web.Application([(r"/(.*)", tornado.web.StaticFileHandler, {"path": path}), ])
+	http_server = tornado.httpserver.HTTPServer(application)
+	http_server.listen(port)
+	tornado.ioloop.IOLoop.instance().start()
