d5c8e0082e142c40f4b64ee5618091b0d9952f06
chrisw <chrisw@soe.ucsc.edu>
2015-01-05 15:11:10 -0800

    cleanup: remove obsolete files

--- observation_deck/scripts/observationData.js
+++
@@ -1,8 +0,0 @@
-/**
- * chrisw@soe.ucsc.edu
- * 23JAN14
- * observationData.js contains observation data on samples/patients over various features/events.
- */
-function observationData() {
-    heatmapData.call(this);
-}
